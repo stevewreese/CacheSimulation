af5ac3fa9397df5874b224409ef7fb379713c035
stevewreese <stephenw.reese@gmail.com>
2016-11-30 11:47:27 -0700

    direct cache

--- directmapped.cpp
+++ directmapped.cpp
@@ -10,13 +10,36 @@ directMapped::directMapped()
 
 void directMapped::cache()
 {
+    hits = 0;
+    misses = 0;
     for(int i = 0; i < 27; i++)
     {
         address = addresses[i];
         tag = address/64;
         row = (address/16)%4;
         offset = address%16;
-        std::cout << "address " << address << " row " << row << " tag " << tag << " offset " << offset << std::endl;
+        //std::cout << "address " << address << " row " << row << " tag " << tag << " offset " << offset << std::endl;
+        if(table[row][0] == 1)
+        {
+            if(table[row][1] == tag)
+            {
+                hits ++;
+                std::cout << "Accessing " << address << "(tag " << tag << "): Hit from row  " << row << std::endl;
+            }
+            else {
+                misses ++;
+                table[row][1] = tag;
+                std::cout << "Accessing " << address << "(tag " << tag << "): miss - cached to row  " << row << std::endl;
+            }
+        }
+        else
+        {
+            misses ++;
+            table[row][0] = 1;
+            table[row][1] = tag;
+            std::cout << "Accessing " << address << "(tag " << tag << "): miss - cached to row  " << row << std::endl;
+        }
+        std::cout << "hits " << hits << " misses " << misses << std::endl;
 
 
 
--- directmapped.h
+++ directmapped.h
@@ -11,8 +11,8 @@ public:
 
 private:
     int addresses [27] = {4, 8, 20, 24, 28, 36, 44, 20, 24, 28, 36, 40, 44, 68, 72, 92, 96, 100, 104, 108, 112, 100, 112, 116, 120, 128, 140};
-    //col 0 isvalid 1 is true 0 is false col 2 tag col 3 offset 4-7 data
-    int table [4] [8];
+    //col 0 isvalid 1 is true 0 is false col 1 tag  col 2 data 96 bytes
+    int table [4] [3];
     int address;
     int hits;
     int misses;
